e983bf73f4566fa0cef4d9b0bc013dbc1ea4a998
Ally Swank - AUB <aubars001@dmcvlogin3.asc.edu>
2022-04-18 13:39:19 -0500

    Script works to trim the reads with trimgalore

--- RNASeq_Data/spring-cleaning.sh
+++ RNASeq_Data/spring-cleaning.sh
@@ -15,13 +15,15 @@
 	#Get read counts per gene
 	#Use the read counts to find differentially expressed genes (DEGs)
 
+#Packages: FastQC, TrimGalore, 
+
 ######################################################################################
 ######################################################################################
 ######################################################################################
 
 #1) Check the quality of the reads
 
-function check_quality {
+function quality_check {
 
 	#input files: *.fastq.gz in ~/RNASeq_Data/Case and ../Control - hardcoded here, but this can be customized by the user.
 	#output files: *.html to be viewed in a web browser
@@ -44,25 +46,67 @@ function check_quality {
 
 function trim_reads {
 
-	#input files: *.fastq.gz in ~/RNASeq_Data/Case and ../Control - hardcoded here, but this can be customized by the user.
+	#input files: *.fastq.gz in ~/s4b-project/RNASeq_Data/Case and ../Control - hardcoded here, but this can be customized by the user.
         #output files: *_trimmed.fq files 
+		#These files will be located in ~/s4b-project/RNASeq_Data/TrimmedReads
 	#packages: trimgalore
-		#trimgalore should cut sequences at illumina adapters
+		#trimgalore will automatically detect and cut sequences at illumina adapters
 
 	###############################################################################
 
 	module load trimgalore/0.6.6 #loading trimgalore module from ASC
 
+	#making new directories in ~/s4b-project/RNASeq_Data for the trimmed data to be stored
+	mkdir TrimmedReads
+	mkdir TrimmedReads/Case
+	mkdir TrimmedReads/Control
+
 	cd Case #move into the directory with the fastq files
-        trim_galore --paired *R1.fastq.gz *R2.fastq.gz
+		#TrimGalore was struggling to pair files, so this had to be hard coded for each pair
+        trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Case 4040-KH-14.4040-KH-14_0_filtered_R1.fastq.gz 4040-KH-14.4040-KH-14_0_filtered_R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Case 4040-KH-16.4040-KH-16_0_filtered_R1.fastq.gz 4040-KH-16.4040-KH-16_0_filtered_R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Case 4040-KH-21.4040-KH-21_0_filtered_R1.fastq.gz 4040-KH-21.4040-KH-21_0_filtered_R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Case 4040-KH-22.4040-KH-22_0_filtered_R1.fastq.gz 4040-KH-22.4040-KH-22_0_filtered_R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Case 4040-KH-23.4040-KH-23_0_filtered_R1.fastq.gz 4040-KH-23.4040-KH-23_0_filtered_R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Case 4040-KH-24.4040-KH-24_0_filtered_R1.fastq.gz 4040-KH-24.4040-KH-24_0_filtered_R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Case 4040-KH-25.4040-KH-25_0_filtered_R1.fastq.gz 4040-KH-25.4040-KH-25_0_filtered_R2.fastq.gz
+
         cd ../Control #move into directory with the control fastq files
-	trim_galore --paired *R1.fastq.gz *R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Control 4040-KH-1.4040-KH-1_0_filtered_R1.fastq.gz 4040-KH-1.4040-KH-1_0_filtered_R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Control 4040-KH-4.4040-KH-4_0_filtered_R1.fastq.gz 4040-KH-4.4040-KH-4_0_filtered_R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Control 4040-KH-5.4040-KH-5_0_filtered_R1.fastq.gz 4040-KH-5.4040-KH-5_0_filtered_R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Control 4040-KH-6.4040-KH-6_0_filtered_R1.fastq.gz 4040-KH-6.4040-KH-6_0_filtered_R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Control 4040-KH-17.4040-KH-17_0_filtered_R1.fastq.gz 4040-KH-17.4040-KH-17_0_filtered_R2.fastq.gz
+	trim_galore --paired --output_dir ~/s4b-project/RNASeq_Data/TrimmedReads/Control 4040-KH-18.4040-KH-18_0_filtered_R1.fastq.gz 4040-KH-18.4040-KH-18_0_filtered_R2.fastq.gz
+
+}
+
+#3) Check the quality of the trimmed reads
+
+function qc_trimmed {
+
+        #input files: *.fastq.gz in ~/s4b-project/RNASeq_Data/TrimmedReads/Case and ../Control - hardcoded here, but this can be customized by the user.
+        #output files: *.html to be viewed in a web browser
+                #This file will be located in ~/s4b-project/RNASeq_Data/TrimmedReads/trimmed-FastQC as specified in the code. This can be modified by the user if th$
+        #Packages: FastQC
+
+        ###############################################################################
+
+        module load fastqc/0.11.9 #loading the fastqc module from ASC
+
+        mkdir trimmed-FastQC #make a new directory for output files
+        
+	cd Case #move into the directory with the fastq files
+        fastqc *.fq.gz -o ~/s4b-project/RNASeq_Data/TrimmedReads/trimmed-FastQC #Use FastQC to perform a quality check of sequences
+        cd ../Control #move into directory with the control fastq files
+        fastqc *.fq.gz -o ~/s4b-project/RNASeq_Data/TrimmedReads/trimmed-FastQC #Use FastQC to perform a quality check of sequences
 
 }
 
 function main {
-#	check_quality /home/RNASeq_Data/
-	trim_reads /home/RNASeq_Data/
+#	quality_check /home/RNASeq_Data/
+#	trim_reads /home/RNASeq_Data/
+	qc_trimmed /home/RNASeq_Data/TrimmedReads
 }
 
 main
